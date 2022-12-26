972b623d3a1572d210d4ba1b9f070a8ac995479e
Zwiqler94 <Zwiqler94@users.noreply.github.com>
2022-12-26 15:58:50 -0500

    chore(init): initial push

---
+++ functions/src/index.ts
@@ -0,0 +1,12 @@
+import { app as api } from "../../src/server";
+import * as functions from "firebase-functions";
+
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+export const usernameGeneratorAPI = functions.https.onRequest(api);
