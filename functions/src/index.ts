bebfd2413048320bb43c6cba125d26b28d91c4bb
Zwiqler94 <Zwiqler94@users.noreply.github.com>
2023-01-01 17:23:33 -0500

    feature(errors): add error handler

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
-import {app as api} from "../../src/server";
+import { app as api } from "../../src/server";
+import { NextFunction, Request, Response } from "express";
 import * as functions from "firebase-functions";
+import cors from "cors";
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -9,6 +11,16 @@ import * as functions from "firebase-functions";
 //   response.send("Hello from Firebase!");
 // });
 
-export const usernameGeneratorAPI = functions
-    .runWith({enforceAppCheck: true})
-    .https.onRequest(api);
+api.use(cors({ origin: true }));
+api.use((req: Request, res: Response, next: NextFunction) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  if (req.method === "OPTIONS") {
+    res.set("Access-Control-Allow-Methods", "POST");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+  }
+});
+
+export const usernameGeneratorAPI = functions.https
+  // .runWith({enforceAppCheck: true})
+  .onRequest(api);
